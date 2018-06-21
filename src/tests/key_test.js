b56821b94700e9c71cb4f68597c6a7c9c0025577
Andrés Tobelem <andres.tobelem@gmail.com>
2018-06-21 18:51:13 -0300

    Remove commons from tests

--- src/tests/key_test.js
+++ src/tests/key_test.js
@@ -1,7 +1,6 @@
 import request from "supertest";
 import assert from "assert";
 import app from "../app";
-import { shouldRespondWithCode200, shouldRespondWithAJSON } from "./commons";
 import Config from "../config";
 
 describe("/key/api", function() {
@@ -9,11 +8,15 @@ describe("/key/api", function() {
 
   describe("#", function() {
     it("GET / should respond with 200", function(done) {
-      shouldRespondWithCode200(baseURI, done);
+      request(app)
+        .get(baseURI)
+        .expect(200, done);
     });
 
     it("GET / should respond with a JSON", function(done) {
-      shouldRespondWithAJSON(baseURI, done);
+      request(app)
+        .get(baseURI)
+        .expect("Content-Type", /json/, done);
     });
   });
 });
--- src/tests/sdk_test.js
+++ src/tests/sdk_test.js
@@ -1,7 +1,6 @@
 import request from "supertest";
 import assert from "assert";
 import app from "../app";
-import { shouldRespondWithCode200, shouldRespondWithAJSON } from "./commons";
 import Config from "../config";
 
 describe("/sdk/api", function() {
@@ -9,15 +8,15 @@ describe("/sdk/api", function() {
 
   describe("#", function() {
     it("GET / should respond with 200", function(done) {
-      shouldRespondWithCode200(baseURI, done);
+      request(app)
+        .get(baseURI)
+        .expect(200, done);
     });
 
     it("GET / should respond with a JSON", function(done) {
-      shouldRespondWithAJSON(baseURI, done);
-    });
-
-    it("GET / should respond with the welcome text", function(done) {
-      shouldRespondWithAJSON(baseURI, done);
+      request(app)
+        .get(baseURI)
+        .expect("Content-Type", /json/, done);
     });
   });
 });
