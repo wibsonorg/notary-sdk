b8f9c1bac2cdbb5b4e7c971c9dad5d962474c9aa
Nico <ncls.ayala@gmail.com>
2018-07-02 16:51:06 -0300

    Add documentation example for /sdk/buyer-api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import cors from "cors";
 import bodyParser from "body-parser";
 import http from "http";
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 // My modules
 import api from "./routes/api";
@@ -24,7 +26,7 @@ function accessLog(err, req, res, next) {
 
 function requestTime(err, req, res, next) {
   var start = Date.now();
-  res.on("header", function() {
+  res.on("header", function () {
     Date.now() - start;
   });
   next();
@@ -57,5 +59,21 @@ app.use(cors());
 // Routes
 
 app.use("/sdk", api);
+
+// API Documentation
+// TODO: To be improved
+const docsAuthentication = async (_req, _res, next) => {
+  console.log("Authenticated for docs");
+  next();
+}
+app.use('/api-docs', docsAuthentication, swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
+  swaggerDefinition: {
+    info: {
+      title: 'Notary SDK', // Title (required)
+      version: '1.0.0', // Version (required)
+    },
+  },
+  apis: ['./src/routes/api.js'], // Path to the API docs
+})));
 
 export default app;
