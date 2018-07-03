3213670c1b5ef986ffc1f92f0026cd3773768dcf
Andrés Tobelem <andres.tobelem@gmail.com>
2018-07-03 19:19:54 -0300

    Adds documentation (#12)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import boom from 'express-boom';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 import { health } from './routes';
 
 const app = express();
@@ -15,5 +18,19 @@ app.use(cors());
 app.use(boom());
 
 app.use('/health', health);
+
+const ls = dir =>
+  fs.readdirSync(dir)
+    .reduce((accumulator, file) => [...accumulator, `src/routes/${file}`], []);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
+  swaggerDefinition: {
+    info: {
+      title: 'Wibson Notary Official SDK',
+      version: '1.0.0',
+    },
+  },
+  apis: ls('src/routes'),
+})));
 
 export default app;
