d38f0f50f88fad482e5ba2f7f89a0477eb807629
Andrés Tobelem <andres.tobelem@gmail.com>
2018-07-10 12:52:10 -0300

    Add the second step of the flow. The buyer can ask for accept audit. Still need to divede in to functions (#17)

--- test/routes/buyers_spec.js
+++ test/routes/buyers_spec.js
@@ -1,7 +1,9 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import ethCrypto from 'eth-crypto';
 import app from '../../src/app';
 // import Config from '../../config';
+
 
 function requestPost(uri, objectParam = {}, handler) {
   request(app)
@@ -18,47 +20,162 @@ function requestGet(uri, handler) {
     .end(handler);
 }
 
-describe('/buyers', () => {
-  const baseURI = '/buyers';
+const baseURI = '/buyers';
 
-  describe('#GET /', () => {
-    it(`-responds with status 200
-        -responds with JSON`, (done) => {
+describe('#GET buyers/', () => {
+  it('responds with status 200', (done) => {
+    requestGet(baseURI, (err, res) => {
+      expect(res.status).to.be.equal(200);
+      done();
+    });
+  });
+});
+
+describe('#GET buyers/', () => {
+  it('responds with JSON', (done) => {
+    requestGet(baseURI, (err, res) => {
+      expect(res.type).to.be.equal('application/json');
+      done();
+    });
+  });
+});
+
+const realDataOrder = 'this-is-a-real-data-order';
+const fakeDataOrder = 'this-is-a-fake-data-order';
+
+const auditUri = `${baseURI}/audit`;
+const auditUriWithDataOrder = `${auditUri}/${realDataOrder}`;
+
+describe('#POST buyers/audit/:dataOrder', () => {
+  context('when the object params is empty', () => {
+    it('responds with status 400', (done) => {
+      requestPost(auditUriWithDataOrder, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.be.equal(400);
+        done();
+        return true;
+      });
+    });
+  });
+});
+
+describe('#POST /buyers/audit/consent/:dataOrder', () => {
+  context('when the dataOrder is a fake dataOrder', () => {
+    it('responds with status 400', (done) => {
+      requestPost(`/buyers/audit/consent/${fakeDataOrder}`, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.be.equal(400);
+        done();
+        return true;
+      });
+    });
+
+    it('responds with JSON', (done) => {
       requestGet(baseURI, (err, res) => {
-        expect(res.status).to.be.equal(200);
         expect(res.type).to.be.equal('application/json');
         done();
       });
     });
   });
+});
 
-  const dataOrder = 'this-is-a-data-order';
-  const auditUri = `${baseURI}/audit`;
-  const auditUriWithDataOrder = `${auditUri}/${dataOrder}`;
+describe('#POST /buyers/audit/consent/:dataOrder', () => {
+  it('responds with JSON', (done) => {
+    requestGet(baseURI, (err, res) => {
+      expect(res.type).to.be.equal('application/json');
+      done();
+    });
+  });
 
-  describe('/buyers/audit', () => {
-    describe('#POST /audit', () => {
-      context('when there is no dataOrder on the URI', () => {
-        it('responds with status 404', (done) => {
-          requestPost(auditUri, {}, (err, res) => {
-            if (err) return done(err);
-            expect(res.status).to.be.equal(404);
-            return done();
-          });
-        });
+  context('when the dataOrder is a real dataOrder', () => {
+    it('responds with status 200', (done) => {
+      requestPost(`/buyers/audit/consent/${realDataOrder}`, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.be.equal(200);
+        done();
+        return true;
       });
     });
 
-    describe('#POST /audit/:dataOrder', () => {
-      context('when the object params is empty', () => {
-        it('responds with status 400', (done) => {
-          requestPost(auditUriWithDataOrder, {}, (err, res) => {
+    it('responds with an object with an orderAdress property', (done) => {
+      requestPost(`/buyers/audit/consent/${realDataOrder}`, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.haveOwnProperty('orderAddress');
+        done();
+        return true;
+      });
+    });
+
+    it(
+      'responds with an object with an responsesPercentage property',
+      (done) => {
+        requestPost(
+          `/buyers/audit/consent/${realDataOrder}`,
+          {}, (err, res) => {
             if (err) return done(err);
-            expect(res.status).to.be.equal(400);
+            expect(res.body).to.haveOwnProperty('responsesPercentage');
             done();
             return true;
-          });
-        });
+          },
+        );
+      },
+    );
+
+    it('responds with an object with an notarizationFee property', (done) => {
+      requestPost(`/buyers/audit/consent/${realDataOrder}`, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.haveOwnProperty('notarizationFee');
+        done();
+        return true;
+      });
+    });
+
+    it(
+      'responds with an object with an notarizationTermsOfService property',
+      (done) => {
+        requestPost(
+          `/buyers/audit/consent/${realDataOrder}`,
+          {}, (err, res) => {
+            if (err) return done(err);
+            expect(res.body).to.haveOwnProperty('notarizationTermsOfService');
+            done();
+            return true;
+          },
+        );
+      },
+    );
+
+    it('responds with an object with an signature property', (done) => {
+      requestPost(`/buyers/audit/consent/${realDataOrder}`, {}, (err, res) => {
+        if (err) return done(err);
+        expect(res.body).to.haveOwnProperty('signature');
+        done();
+        return true;
+      });
+    });
+
+    it('responds with a correct signature', (done) => {
+      requestPost(`/buyers/audit/consent/${realDataOrder}`, {}, (err, res) => {
+        const orderAddress = '0xd56359a66d8ef7329507e5dd1f40aef7cd3320e7';
+        const responsesPercentage = 30;
+        const notarizationFee = 2;
+        const notarizationTermsOfService = 'The terms of service';
+        const notaryPrivateKey
+        = '0x107be946709e41b7895eea9f2dacf998a0a9124acbb786f0fd1a826101581a07';
+
+        const message = [
+          orderAddress,
+          responsesPercentage,
+          notarizationFee,
+          notarizationTermsOfService,
+        ];
+        const messageHash = ethCrypto.hash.keccak256(message);
+        const signature = ethCrypto.sign(notaryPrivateKey, messageHash);
+
+        if (err) return done(err);
+        expect(res.body.signature).to.be.equal(signature);
+        done();
+        return true;
       });
     });
   });
