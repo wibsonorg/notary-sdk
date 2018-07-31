5113ac6a15473a83eec4eab753cb0ee43935d10f
Daniel Fernandez <daniel@grandata.com>
2018-07-30 22:27:47 -0300

    Unnecessary config variables removed

--- notary-signing-service/config/index.js
+++ notary-signing-service/config/index.js
@@ -14,29 +14,14 @@ var config = {
   env: env.NODE_ENV,
   port: env.PORT,
   host: env.HOST,
-  // TEST
-  privateKey: env.PRIVATE_KEY,
-  orderAddress: env.ORDER_ADDRESS,
-  responsesPercentage: env.RESPONSES_PERCENTAGE,
-  notarizationFee: env.NOTARIZATION_FEE,
-  notarizationTermsOfService: env.NOTARIZATION_TERMS_OF_SERVICE,
-  signature: env.SIGNATURE,
 
-  cache: {
-    enabled: env.CACHE === 'enabled',
-    adapter: env.CACHE_ADAPTER
-  },
   logType: env.LOG_TYPE,
   log: {
     error: env.ERROR_LOG,
     combined: env.COMBINED_LOG
   },
-  web3: {
-    provider: env.WEB3_PROVIDER
-  },
-  redis: {
-    socket: env.REDIS_SOCKET
-  },
+
+  privateKey: env.PRIVATE_KEY,
 };
 
 exports.default = config;
