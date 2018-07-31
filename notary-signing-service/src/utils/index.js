313ad651a679038c14be186d48b73767799c56fa
Daniel Fernandez <daniel@grandata.com>
2018-07-30 22:20:37 -0300

    Unnecessary redis and level removed

--- notary-signing-service/src/utils/index.js
+++ notary-signing-service/src/utils/index.js
@@ -1,6 +1,3 @@
-import web3 from './web3';
-import cache from './cache';
 import logger from './logger';
 
-export { web3, cache, logger };
-export { createRedisStore, createLevelStore } from './storage';
+export { logger }; // eslint-disable-line
--- notary-signing-service/src/utils/web3.js
+++
@@ -1,6 +0,0 @@
-import Web3 from 'web3';
-import config from '../../config';
-
-const web3 = new Web3(new Web3.providers.HttpProvider(config.web3.provider));
-
-export default web3;
