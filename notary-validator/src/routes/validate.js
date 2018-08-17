8876238ba9df5a4e3c9dcd433fd940d14f7c5d0d
Andrés Tobelem <andres.tobelem@gmail.com>
2018-08-17 06:16:51 +0200

    Reach mobile connect!!!

--- notary-validator/src/routes/validate.js
+++ notary-validator/src/routes/validate.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import requestPromise from 'request-promise-native';
+import axios from 'axios';
 import config from '../../config';
-import { web3, cache } from '../utils';
 
 const router = express.Router();
 
@@ -19,9 +18,12 @@ const router = express.Router();
  *         description: When the app is OK
  */
 router.get('/:MSISDN', async (req, res) => {
-  const { MSISDN } = req.params;
-  console.log(MSISDN);
-  res.json({ status: 'OK' });
+  try {
+    const { MSISDN } = req.params;
+    await axios.get(' https://mobileconnect.telefonica.es/es/oauth2/authorize?scope=openid%20phone&response_type=code&client_id=ce9c1d81-9191-4082-a954-bbfdc7e0cd93&acr_values=2&state=eee40fe4-8a8d-4b26-8dfd-2fb4d52aa361&redirect_uri=https%3A%2F%2Fwibson-notary.elevenpaths.com%2Fdatavalidator%2Fidentity-callback&nonce=878aacd7-83f1-4b4f-9582-7347b0a6fa61&login_hint=MSISDN%3A34689435912');
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 export default router;
