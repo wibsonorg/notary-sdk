b8106a201e47a80a7f18d1faa60354a288041ce3
Andrés Tobelem <andres.tobelem@gmail.com>
2018-08-19 18:37:51 +0200

    Validation working

--- notary-validator/src/routes/validate.js
+++ notary-validator/src/routes/validate.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import axios from 'axios';
+import uuidv4 from 'uuid/v4';
 import config from '../../config';
 
 const router = express.Router();
@@ -17,13 +18,31 @@ const router = express.Router();
  *       200:
  *         description: When the app is OK
  */
-router.get('/:MSISDN', async (req, res) => {
-  try {
-    const { MSISDN } = req.params;
-    await axios.get(' https://mobileconnect.telefonica.es/es/oauth2/authorize?scope=openid%20phone&response_type=code&client_id=ce9c1d81-9191-4082-a954-bbfdc7e0cd93&acr_values=2&state=eee40fe4-8a8d-4b26-8dfd-2fb4d52aa361&redirect_uri=https%3A%2F%2Fwibson-notary.elevenpaths.com%2Fdatavalidator%2Fidentity-callback&nonce=878aacd7-83f1-4b4f-9582-7347b0a6fa61&login_hint=MSISDN%3A34689435912');
-  } catch (error) {
-    res.json(error);
-  }
+
+router.get('/:MSISDN', (req, res) => {
+  const { MSISDN } = req.params;
+  const nonce = uuidv4();
+  const state = uuidv4();
+  const { clientId, redirectURI } = config;
+
+  axios.get('https://mobileconnect.telefonica.es/es/oauth2/authorize?' +
+  'scope=openid%20phone&' +
+  'response_type=code&' +
+  'acr_values=2&' +
+  `client_id=${clientId}&` +
+  `state=${state}&` +
+  `nonce=${nonce}&` +
+  `redirect_uri=${redirectURI}&` +
+  `login_hint=MSISDN%3A${MSISDN}&` +
+  'version=mc_di_r2_v2.3').then((response) => {
+    res.send(response);
+  })
+    .catch((error) => {
+      res.send(error);
+    })
+    .then(() => {
+    // always executed
+    });
 });
 
 export default router;
