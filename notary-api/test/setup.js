4c0bc3c5edab43cde5f86447ddedf47e33b1c72d
Nico <ncls.ayala@gmail.com>
2018-08-28 20:10:18 -0300

    Add missing env variables

--- notary-api/test/setup.js
+++ notary-api/test/setup.js
@@ -1,5 +1,12 @@
 // TODO: This is a temporal solution, this file should not exist
+process.env.ORDER_ADDRESS = '0xaa8f666e26599f21cccf10cd560c400ddc6be388';
+process.env.WEB3_PROVIDER = 'wss://ropsten.infura.io/ws';
 process.env.NOTARIZATION_RESULTS_STORE_PATH = '/tmp/notarization_results';
+process.env.DATA_VALIDATION_RESULTS_STORE_PATH = '/tmp/data_validation_results';
 process.env.RESPONSES_PERCENTAGE = 30;
 process.env.NOTARIZATION_FEE = 4;
 process.env.NOTARIZATION_TERMS_OF_SERVICE = 'Terms of Service';
+process.env.STORAGE_URL = 's3://storage.wibson.org';
+process.env.STORAGE_REGION = 'eu-central-1';
+process.env.STORAGE_BUCKET = 'wibson-storage';
+process.env.SIGNATURE = '0x2e58ea30aa3d31d4a997a14228c94f7384fab28d2bb6931b648cd0fe7d533fe3634164c7f84b6013d674a93e3c82bab150886767bf56c91cda028191162cd07500';
