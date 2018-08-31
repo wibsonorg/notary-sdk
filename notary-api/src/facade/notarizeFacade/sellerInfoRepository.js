7b89c8c7cdde1eb2fcbd895a14f9a2018658169d
Nico <ncls.ayala@gmail.com>
2018-08-31 17:09:52 -0300

    Remove web3-utils from NAPI. Remove web3 from NOSS

--- notary-api/src/facade/notarizeFacade/sellerInfoRepository.js
+++ notary-api/src/facade/notarizeFacade/sellerInfoRepository.js
@@ -1,6 +1,5 @@
-import web3Utils from 'web3-utils';
 import { dateOrNull } from '../helpers/date';
-import { dataOrderAt } from '../../utils';
+import { web3, dataOrderAt } from '../../utils';
 
 export const fetchSellerInfo = async (orderAddress, sellerAddress) => {
   const dataOrder = dataOrderAt(orderAddress);
@@ -15,7 +14,7 @@ export const fetchSellerInfo = async (orderAddress, sellerAddress) => {
       dataHash: sellerInfo[2],
       createdAt: dateOrNull(sellerInfo[3]),
       closedAt: dateOrNull(sellerInfo[4]),
-      status: web3Utils.hexToUtf8(sellerInfo[5]),
+      status: web3.utils.hexToUtf8(sellerInfo[5]),
     };
   } catch (error) {
     return { error: error.message };
