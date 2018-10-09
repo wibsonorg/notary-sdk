025f77f41d99ab5dcd28d48c5f04588989a44c01
Andrés Tobelem <andres.tobelem@gmail.com>
2018-10-09 18:17:46 -0300

    web3

--- notary-api/src/utils/web3.js
+++ notary-api/src/utils/web3.js
@@ -1,7 +1,6 @@
 import Web3 from 'web3';
-import WebsocketProvider from 'web3-providers-ws';
 import config from '../../config';
 
-const web3 = new Web3(new WebsocketProvider(config.web3.provider));
+const web3 = new Web3(config.web3.provider);
 
 export default web3;
