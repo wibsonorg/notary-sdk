a937e0c0c41ba0f9f3f59fbd835054c672caf613
Daniel Fernandez <daniel@grandata.com>
2018-10-11 21:32:13 -0300

    Add host to server

--- notary-api/src/server.js
+++ notary-api/src/server.js
@@ -5,9 +5,9 @@ import logger from './utils/logger';
 import listenContractEvents from './contractEventSubscribers';
 
 const server = async () => {
-  const { port, env } = config;
-  app.listen(port, () =>
-    logger.info(`Notary API listening on port ${port} in ${env} mode`));
+  const { port, host, env } = config;
+  app.listen({ port, host }, () =>
+    logger.info(`Notary API listening on port ${port} and host ${host} in ${env} mode`));
 
   listenContractEvents(app.locals.stores);
 };
