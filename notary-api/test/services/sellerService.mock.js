b83fe6b80ea55eaf9d061ae2da9fa35245f31dc6
pabloaguayo <pablo.aguayo@soluciones3f.com.ar>
2019-02-11 10:07:59 -0300

    improvement in db fetch

--- notary-api/test/services/sellerService.mock.js
+++ notary-api/test/services/sellerService.mock.js
@@ -2,7 +2,7 @@ import td from 'testdouble';
 import sinon from 'sinon';
 import test from 'ava';
 
-export const sellers = { get: sinon.stub(), put: sinon.spy() };
+export const sellers = { safeFetch: sinon.stub(), put: sinon.spy() };
 td.replace('../../src/utils/stores', { sellers });
 
 function returnSeller(address) {
@@ -11,6 +11,6 @@ function returnSeller(address) {
 }
 
 test.beforeEach(() => {
-  sellers.get.callsFake(returnSeller);
+  sellers.safeFetch.callsFake(returnSeller);
 });
 test.afterEach(sinon.reset);
