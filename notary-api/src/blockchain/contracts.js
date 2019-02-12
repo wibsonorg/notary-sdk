3c89791814810d63d5274782eeaee4a90a336b17
Facundo D <genuinefafa@gmail.com>
2019-02-12 14:35:57 -0300

    delete unused lines ask by @nicoayala

--- notary-api/src/blockchain/contracts.js
+++ notary-api/src/blockchain/contracts.js
@@ -1,16 +1,12 @@
 import { web3 } from '../utils/web3';
 import config from '../../config';
 
-import WibcoinDefinition from '../../contracts/Wibcoin.json';
 import DataExchangeDefinition from '../../contracts/DataExchange.json';
-import BatPayDefinition from '../../contracts/BatPay.json';
 
 const { Contract } = web3.eth;
-const { wibcoin, dataExchange, batPay } = config.contracts.addresses;
+const { dataExchange } = config.contracts.addresses;
 
-export const Wibcoin = new Contract(WibcoinDefinition.abi, wibcoin);
 export const DataExchange = new Contract(DataExchangeDefinition.abi, dataExchange);
-export const BatPay = new Contract(BatPayDefinition.abi, batPay);
 
 export const toDate = ts => (ts > 0 ? new Date(ts * 1000).toISOString() : null);
 
