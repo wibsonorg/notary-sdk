686b42fad8fd93e14b800ad7e308becc4b4999ee
Nahuel Santoalla <nahjersan@gmail.com>
2019-04-24 12:25:08 -0300

    fix NotarizationRequestSeller swagger-docs

--- notary-api/src/routes/buyers.js
+++ notary-api/src/routes/buyers.js
@@ -216,22 +216,7 @@ router.post(
  *         name: notarizationRequest
  *         required: true
  *         schema:
- *           required:
- *             - orderId
- *             - sellers
- *             - callbackUrl
- *           properties:
- *             orderId:
- *               type: integer
- *               description: The unique identifier for the order
- *             sellers:
- *               type: array
- *               description: List of sellers send to notarize
- *               items:
- *                 type: string
- *             callbackUrl:
- *               type: string
- *               description: Buyer's URL to call after notarization
+ *           $ref: "#/definitions/NotarizationRequest"
  *     produces:
  *       - application/json
  *     responses:
@@ -239,6 +224,42 @@ router.post(
  *         description: When the job was successfully added.
  *       500:
  *         description: When there was an error with the app
+ *
+ * definitions:
+ *   NotarizationRequest:
+ *     type: object
+ *     required:
+ *       - orderId
+ *       - sellers
+ *       - callbackUrl
+ *     properties:
+ *       orderId:
+ *         type: integer
+ *         description: The unique identifier for the order
+ *       sellers:
+ *         type: array
+ *         description: List of sellers send to notarize
+ *         items:
+ *           $ref: "#/definitions/NotarizationRequestSeller"
+ *       callbackUrl:
+ *         type: string
+ *         description: Buyer's URL to call after notarization
+ *   NotarizationRequestSeller:
+ *     type: object
+ *     required:
+ *       - address
+ *       - id
+ *       - decryptionKeyHash
+ *     properties:
+ *       address:
+ *         type: string
+ *         description: Seller's ethereum address
+ *       id:
+ *         type: number
+ *         description: Seller ID in the DataExchange contract
+ *       decryptionKeyHash:
+ *         type: string
+ *         description: Hash of the key that decrypts the information
  */
 router.post(
   '/notarization-request',
