2ec8fbb358a5e6a5a32ebdf364aa3fd0360926d7
Agus <agustinmodugno@gmail.com>
2019-04-30 13:49:41 -0300

    Update notary-api/src/jobs/payments.js
    Co-Authored-By: CarrilloJuan <36606947+CarrilloJuan@users.noreply.github.com>

--- notary-api/src/jobs/payments.js
+++ notary-api/src/jobs/payments.js
@@ -38,7 +38,7 @@ export async function sendUnlock(payIndex, registerPaymentHash) {
   const { price } = await getDataOrder(orderId);
   validateOrThrow(registerPayment.amount === '10000', 'amount did not match price');
   // eslint-disable-next-line no-mixed-operators
-  const fee = price * request.sellers.length * (notarizationPercentage / 100) + notarizationFee;
+  const fee = price * sellers.length * (notarizationPercentage / 100) + notarizationFee;
   validateOrThrow(Number(registerPayment.fee) === fee, 'fee did not match requested fee');
   await axios.post(
     `${brokerUrl}/unlock`,
