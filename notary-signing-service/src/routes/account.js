23bad0a16d53e9e0d063e90c5c833d9460fad87f
Daniel Fernandez <daniel@grandata.com>
2019-05-03 15:58:31 -0300

    Signing service signed info endpoint

--- notary-signing-service/src/routes/account.js
+++ notary-signing-service/src/routes/account.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { account } from '../utils';
+import { signPayload } from '../utils/wibson-lib/cryptography';
 
 const router = express.Router();
 
@@ -18,6 +19,48 @@ router.get('/', async (_req, res) => {
   res.json({
     address: account.getAddress(),
     publicKey: account.getPublicKey(),
+  });
+});
+
+/**
+ * @swagger
+ * /account/info:
+ *   post:
+ *     description: Signs specific notary info
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: When the notary info could be correctly signed
+ */
+router.post('/info', async (req, res) => {
+  const address = account.getAddress();
+  const publicKey = account.getPublicKey();
+  const {
+    name, notarizationUrl, dataResponsesUrl, headsUpUrl,
+  } = req.body;
+
+  const privateKey = account.getPrivateKey();
+
+  // you cannot spread an object in varargs
+  const signature = signPayload(
+    privateKey,
+    name,
+    address,
+    notarizationUrl,
+    dataResponsesUrl,
+    headsUpUrl,
+    publicKey,
+  );
+
+  res.json({
+    name,
+    address,
+    notarizationUrl,
+    dataResponsesUrl,
+    headsUpUrl,
+    publicKey,
+    signature,
   });
 });
 
