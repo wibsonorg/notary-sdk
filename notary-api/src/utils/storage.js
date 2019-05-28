c49d37f3504eeba7b6e2084614cfd1d3412e112b
Nico <ncls.ayala@gmail.com>
2019-05-28 13:10:52 -0300

    Update level store interface

--- notary-api/src/utils/storage.js
+++ notary-api/src/utils/storage.js
@@ -2,6 +2,8 @@ import asyncRedis from 'async-redis';
 import redis from 'redis';
 import level from 'level';
 import config from '../../config';
+import logger from './logger';
+import { lock } from './lock';
 
 const { url, prefix } = config.redis;
 export const createRedisStore = ns =>
@@ -9,16 +11,23 @@ export const createRedisStore = ns =>
 
 /**
  * @typedef LevelStore A store that uses LevelDB
- * @property {(id: K) => Promise<V>} fetch Retrives the value parsed from LevelDB
+ * @property {(id: K) => Promise<V>} fetch Retrieves the value parsed from LevelDB
  * @property {(id: K) => Promise<V>} safeFetch As fetch but retrives null if missing
- * @property {(id: K, obj: V) => Promise<void>} store Stores the value stringified on LevelDB
- * @property {(id: K, obj: V) => Promise<void>} update Updates the value stringified on LevelDB
+ * @property {(id: K, obj: V) => Promise<V>} store Stores the value stringified on LevelDB
+ * @property {(id: K) => Promise<V>} Removes data from the store.
+ * @property {(list: [K, V][]) => Promise<{Object<string, V>}>} storeList
+ *    Stores a list of [key, value]
+ * @property {(id: K, obj: V) => Promise<boolean>} storeNonExistant
+ *    Stores the value stringified on LevelDB, only if it doesnt exists returns true if stored
+ * @property {(id: K, obj: V) => Promise<V>} update Updates the value stringified on LevelDB
+ * @property {(id: K, value: V, defaultOldValue: V) => Promise<V>} storeGreatest
+ *    Stores the greatest value
  * @property {(group: string) => Promise<({id:K}&V)[]>} list Lists all the objects stored on LevelDB
  * @property {(group: string) => Promise<K[]>} listKeys Lists all the keys stored on LevelDB
  * @property {(group: string) => Promise<V[]>} listValues Lists all the values stored on LevelDB
  * @template K
  * @template V
-*/
+ */
 /**
  * @function createLevelStore Creates a level store
  * @param {string} dir Path to the store, starting on {config.levelDirectory}
@@ -26,39 +35,92 @@ export const createRedisStore = ns =>
  */
 export const createLevelStore = (dir) => {
   const store = level(`${config.levelDirectory}/${dir}`, (err, db) => {
-    if (err) { throw new Error(err); }
+    if (err) {
+      throw new Error(err);
+    }
     return db;
   });
-  store.fetch = async id => JSON.parse(await store.get(id));
-  store.safeFetch = async (id, defaultResponse = null) => {
+  store.fetch = async (id) => {
+    try {
+      return JSON.parse(await store.get(id));
+    } catch (e) {
+      throw new Error(`${dir} :: ${e.message}`);
+    }
+  };
+  store.safeFetch = async (id, defaultValue) => {
     try {
       return await store.fetch(id);
     } catch (_) {
-      return defaultResponse;
+      return defaultValue;
     }
   };
-  store.store = (id, obj) => store.put(id, JSON.stringify(obj));
-  store.update = async (id, obj) => {
-    const value = await store.fetch(id);
-    return store.store(id, { ...value, ...obj });
-  };
-  const createListFunction = mode => group => new Promise((res, rej) => {
-    const result = [];
-    store.createReadStream({
-      keys: mode !== 'values',
-      values: mode !== 'keys',
-      ...(group ? { gte: group, lte: `${group}∞` } : {}),
-    })
-      .on('data', data => result.push(data))
-      .on('error', rej)
-      .on('end', () => res(result));
-  }).then((list) => {
-    switch (mode) {
-      case 'keys': return list;
-      case 'values': return list.map(value => JSON.parse(value));
-      default: return list.map(({ key, value }) => ({ id: key, ...JSON.parse(value) }));
+  store.store = async (id, obj) => {
+    try {
+      await store.put(id, JSON.stringify(obj));
+      return obj;
+    } catch (e) {
+      logger.error(`${dir} :: Can't store value ${JSON.stringify(obj)} for key ${id}`);
+      throw e;
     }
-  });
+  };
+  store.storeNonExistent = (id, mutations) =>
+    lock([[store, id]], async () => {
+      if (await store.safeFetch(id)) return false;
+      const newValue = typeof mutations === 'function' ? await mutations() : mutations;
+      await store.store(id, newValue);
+      return true;
+    });
+  store.update = (id, mutations, defaultValue) =>
+    lock([[store, id]], async () => {
+      const oldValue = await store.safeFetch(id, defaultValue);
+      const newValue = typeof mutations === 'function' ? await mutations(oldValue) : mutations;
+      return store.store(
+        id,
+        typeof newValue === 'object' || newValue === undefined
+          ? {
+            ...oldValue,
+            ...newValue,
+          }
+          : newValue,
+      );
+    });
+  store.storeGreatest = (id, newValue, defaultOldValue = 0) =>
+    store.update(id, oldValue => Math.max(oldValue, newValue), defaultOldValue);
+  store.storeList = async (list) => {
+    await list.reduce((batch, [k, v]) => batch.put(k, JSON.stringify(v)), store.batch()).write();
+    return list.reduce((obj, [k, v]) => ({ ...obj, [k]: v }), {});
+  };
+  store.delete = async (id) => {
+    try {
+      await store.del(id);
+      return true;
+    } catch (e) {
+      logger.error(`${dir} :: Can't delete value for key ${id}`);
+      throw e;
+    }
+  };
+  const createListFunction = mode => group =>
+    new Promise((res, rej) => {
+      const result = [];
+      store
+        .createReadStream({
+          keys: mode !== 'values',
+          values: mode !== 'keys',
+          ...(group ? { gte: group, lte: `${group}∞` } : {}),
+        })
+        .on('data', data => result.push(data))
+        .on('error', rej)
+        .on('end', () => res(result));
+    }).then((list) => {
+      switch (mode) {
+        case 'keys':
+          return list;
+        case 'values':
+          return list.map(value => JSON.parse(value));
+        default:
+          return list.map(({ key, value }) => ({ id: key, ...JSON.parse(value) }));
+      }
+    });
   store.list = createListFunction('all');
   store.listKeys = createListFunction('keys');
   store.listValues = createListFunction('values');
