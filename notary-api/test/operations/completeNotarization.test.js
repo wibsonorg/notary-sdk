3854d3a46cd68ba473877cafa90b016fd38ad285
Nico <ncls.ayala@gmail.com>
2019-06-06 14:09:23 -0300

    Fix tests

--- notary-api/test/operations/completeNotarization.test.js
+++ notary-api/test/operations/completeNotarization.test.js
@@ -6,10 +6,10 @@ import {
 import { completeNotarization } from '../../src/operations/completeNotarization';
 
 const lock = '0x7b9a465acbec4bbce6782e7b2f00d2c6e95c49eff319f1748c1119dc9393a2f6';
-const result = {
-  '0xSellerA': { identified: true },
-  '0xSellerB': { identified: 'true' },
-};
+const result = [
+  { id: 10, identified: true },
+  { id: 20, identified: 'true' },
+];
 
 it('Updates notarization and enqueues job for response', async (assert) => {
   await completeNotarization(lock, result);
