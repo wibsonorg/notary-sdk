36446427f848f75ca9e5ec1f29461f6036411d6a
Nahuel Santoalla <nahjersan@gmail.com>
2019-08-15 17:57:54 -0300

    [migration] set notarizations.status <- responded

--- notary-api/scripts/migrate-batpay.js
+++ notary-api/scripts/migrate-batpay.js
@@ -9,11 +9,12 @@ const {
   //   dataOrders,
   //   eventBlocks,
   // KEEP
-  //   notarizationResults, // will flush
   //   dataResponses, // will flush
   // CLEAR
   sellers, // new BatPay == new ids
   sellersByPayIndex, // payIndex is the key
+  // UPDATE
+  notarizationResults, // ['accepted', 'validating'].includes(r.status) -> { status: 'responded' }
 } = require('../src/utils/stores');
 const logger = require('../src/utils/logger');
 
@@ -21,6 +22,16 @@ const celarDB = async db => db.deleteList(await db.listKeys());
 const isEmpty = async db => (await db.listKeys()).length === 0;
 
 async function migrate() {
+  logger.info('UPDATE NOTARIZATION RESULTS...');
+  await Promise.all((await notarizationResults.list())
+    .filter(r => ['accepted', 'validating'].includes(r.status))
+    .map(r => notarizationResults.update(r.id, {
+      status: 'responded', statusReason: 'Status set in migration to BatPay v2.1',
+    })));
+  logger.info('VERIFYING NOTARIZATION RESULTS...');
+  const results = await notarizationResults.list();
+  const respondedResults = results.filter(a => a.status === 'responded').length;
+  logger.info(`RESPONDED NOTARIZATION RESULTS: ${respondedResults}/${results.length}`);
   logger.info('CLEAR DBS...');
   const dbsToClear = [
     sellers,
